34e7373f4d62adf5bded23f6b04a6ef4f2a58be1
Carlos-Eduardo-developer <164267886+Carlos-Eduardo-developer@users.noreply.github.com>
2024-11-10 20:29:43 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import GaleriaDeImagensVitor from './components/GaleriaDeImagensVitor'
+
+
+function App() {
+  return (
+    <div>
+      <GaleriaDeImagensVitor />
+    </div>
+  )
+}
+
+export default App
